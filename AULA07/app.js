3381f5d056002f1ca40aee319f7245b990c7f445
GENIVANIA MACEDO OLIVEIRA <22282083@CFP127ALUNO.LOCAL>
2023-04-14 10:33:57 -0300

    adicionao readme

--- AULA07/app.js
+++ AULA07/app.js
@@ -1,7 +1,59 @@
 /*****************************************************************************
- * Objetivo: 
+ * Objetivo: API para interagir com o banco (GET, POST, DELETE, PUT)
  * Autor: Genivania Macedo
  * Data: 14/04/2023
  * Versão: 1.0
- * 
  ****************************************************************************/
+
+ //Import das bibliotecas do projeto
+ const express = require('express');
+ const cors = require('cors');
+ const bodyParser = require('bodyParser');
+const { request, response } = require('express');
+
+
+ //Cria o objeto app utilizando a classe do express
+const app = express();
+
+app.use((request,require, next)=>{
+    //Permissões de origin da requisição
+    response.header('Access-Control-Allow-Origin', '*')
+
+    //Permissões de metodos que serao utilizados na api
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+
+    //Define as perissões para o cors
+    app.use(cors())
+
+    //Contunua para a leitura dos EndPoints
+    next();
+
+});
+
+//CRUD (Create, Read, Update e Delete)
+
+
+
+//EndPoint: Retorna todos os dados de alunos
+app.get('/v1/lion-school/aluno', cors(), async function(request,response){
+
+
+});
+
+//EndPoint: Retorna dado do alunos pelo ID
+app.post('/v1/lion-school/aluno/:id', cors(), async function(request,response){
+
+    
+});
+
+//EndPoint: Atualiza o aluno peli ID
+app.put('/v1/lion-school/aluno/:id', cors(), async function(request,response){
+
+    
+});
+
+//EndPoint: Excluir o aluno peli ID
+app.delete('/v1/lion-school/aluno/:id', cors(), async function(request,response){
+
+    
+});
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# back-end_marcel
-# aula_back-end
-# aula_back-end
+# AULAS DE BACK END
+
+AULA | DESCRIÇÃO
+---|---
+ 1 | Introdução a matéria
+ 2 | Realizar a média escolar dos alunos
+ 3 | Criar uma calculadora
+ 4 | Explicação de como realizar calculos matemáticos
+ 5 | Realizar cálculos de uma tabuada tendo duas entardas e o contador
+ 6 | Criar um a API para manipulação de dados de Estados e Cidades
+ 7 | API para interagir com o banco (GET, POST, DELETE, PUT)
+
